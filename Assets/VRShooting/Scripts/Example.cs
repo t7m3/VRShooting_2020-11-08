b7491ab312f938a7031f0ca89283e03c2b4c347e
tmk32 <toda7.mk3@gmail.com>
2020-11-16 19:44:21 +0900

    2020-11-16(月)　3回目のコミット　敵がランダムに出現する。

--- Assets/VRShooting/Scripts/Example.cs
+++ Assets/VRShooting/Scripts/Example.cs
@@ -7,12 +7,18 @@ public class Example : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        // 最初に一度メッセージを表示する
+        Debug.Log("[Start]");  
     }
 
     // Update is called once per frame
     void Update()
     {
+        // Spaceキーが押されている間メッセージを表示する
+        if (Input.GetKey(KeyCode.Space))
+        {
+            Debug.Log("[Update] スペースキーが押されている。");
+        }
         
     }
 }
