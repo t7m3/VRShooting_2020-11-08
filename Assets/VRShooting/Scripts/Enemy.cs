fafb1d740535d1a2256f60760cb0761307c8b01f
tmk32 <toda7.mk3@gmail.com>
2020-11-16 13:07:03 +0900

    2020-11-16(月)　2回目のコミット　衝突すると、敵も破棄される。（弾も、破棄される。）

---
+++ Assets/VRShooting/Scripts/Enemy.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    // OnHitBulletメッセージから呼び出されること想定
+    void OnHitBullet()
+    {
+        // 自身のゲームオブジェクトを破棄
+        Destroy(gameObject);
+    }
+}
