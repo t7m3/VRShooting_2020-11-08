e72f27eaf2866ee8b1773433379f2da18be0c288
tmk32 <toda7.mk3@gmail.com>
2020-11-23 14:56:30 +0900

    2020-11-23(月)　3回目のコミット　スコア表示するようにした。（敵を倒すと1点。）

--- Assets/VRShooting/Scripts/Enemy.cs
+++ Assets/VRShooting/Scripts/Enemy.cs
@@ -4,10 +4,16 @@ using UnityEngine;
 
 public class Enemy : MonoBehaviour
 {
+    [SerializeField] int point = 1;  // 倒したときのスコアポイント
+    Score score;                     // スコア
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        // ゲームオブジェクトを検索
+        var gameObj = GameObject.FindWithTag("Score");
+        // gameObj に含まれる Scoreコンポーネントを取得
+        score = gameObj.GetComponent<Score>();
     }
 
     // Update is called once per frame
@@ -20,6 +26,20 @@ public class Enemy : MonoBehaviour
     void OnHitBullet()
     {
         // 自身のゲームオブジェクトを破棄
+        //stroy(gameObject);
+
+        // 死亡時処理
+        GoDown();
+    }
+
+    // 死亡時処理
+    void GoDown()
+    {
+        //スコアを加算
+        score.AddScore(point);
+
+        // 自身のゲームオブジェクトを一定時間後に破棄
+        //Destroy(gameObject, 1f);
         Destroy(gameObject);
     }
 }
---
+++ Assets/VRShooting/Scripts/Score.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Score : MonoBehaviour
+{
+    Text uiText;  // UITextコンポーネント
+    public int Points { get; private set; }  // 現在のスコアポイント
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        uiText = GetComponent<Text>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void AddScore(int addPoint)
+    {
+        // 現在のポイントに加算
+        Points += addPoint;
+
+        // 得点の更新
+        uiText.text = string.Format("得点：{0:D3}点", Points);
+    }
+}
